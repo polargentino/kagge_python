d564f61808211162f939ecce1a70af02ddf21c3e
Pablo Matías Monsalvo <polargentino@gmail.com>
2025-02-06 19:24:32 -0300

    borre archivo de pruebas individuales salidas.py

--- salidas.py
+++ salidas.py
@@ -1,13 +1,1 @@
-# Otro ejemplo
-def f(x):
-    if x > 0:
-        # Sólo se imprime cuando x es positivo
-        print("Only printed when x is positive; x =", x) # Only printed when x is positive; x = 1
-        # También sólo se imprime cuando x es positivo
-        print("Also only printed when x is positive; x =", x) # Also only printed when x is positive; x = 1
-    # Siempre impreso, independientemente del valor de x
-    print("Always printed, regardless of x's value; x =", x) # Always printed, regardless of x's value; x = 1
-                                                             # Always printed, regardless of x's value; x = 0
 
-f(1)
-f(0)
